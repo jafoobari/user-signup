4e4ae32e551563ef1b1468710300835d70d52bb3
Jabari A <Jabari.Allen@gmail.com>
2017-02-02 08:36:30 -0600

    Added user input verification and welcome page.

--- main.py
+++ main.py
@@ -15,6 +15,8 @@
 # limitations under the License.
 #
 import webapp2
+import re
+import cgi
 
 # html boilerplate for the top of every page
 page_header = """
@@ -42,27 +44,27 @@ page_footer = """
 </html>
 """
 
-def build_page(username,email):
+def build_page(username,email,error_username,error_password,error_verify,error_email):
 
     username_row = '''<td><label for="username">Username</label></td>
                         <td>
                             <input name="username" type="text" value="''' + username + '''"required>
-                            <span class="error"></span>
+                            <span class="error">'''+ error_username +'''</span>
                         </td>'''
     password_row = '''<td><label for="password">Password</label></td>
                         <td>
                             <input name="password" type="password" required>
-                            <span class="error"></span>
+                            <span class="error">'''+ error_password +'''</span>
                         </td>'''
     verify_row = '''<td><label for="verify">Verify Password</label></td>
                         <td>
                             <input name="verify" type="password" required>
-                            <span class="error"></span>
+                            <span class="error">'''+ error_verify +'''</span>
                         </td>'''
     email_row = '''<td><label for="email">Email (optional)</label></td>
                         <td>
                             <input name="email" type="email" value=''' + email + '''>
-                            <span class="error"></span>
+                            <span class="error">'''+ error_email +'''</span>
                         </td>'''
 
     form_table = '''<form method="post">
@@ -93,20 +95,74 @@ def build_page(username,email):
 
     return page_header + form_table + page_footer
 
+username_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and username_re.match(username)
+
+pass_re = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and pass_re.match(password)
+
+email_re  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or email_re.match(email)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
-        content = build_page("","")
+        content = build_page("","","","","","")
+
+        self.response.write(content)
+
+    def post(self):
+
+        error_username = ""
+        error_password = ""
+        error_verify = ""
+        error_email = ""
+
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        if not valid_username(username):
+            error_username = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            error_password = "That wasn't a valid password."
+            have_error = True
+
+        elif password != verify:
+            error_verify = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(email):
+            error_email = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+
+            content = build_page(username,email,error_username,error_password,error_verify,error_email)
+
+            self.response.write(content)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+
+
+class WelcomeHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        content = '''<h1>Welcome, ''' + cgi.escape(username, quote=True) + '''!</h1>'''
 
         self.response.write(content)
 
 
-# class WelcomeHandler(webapp2.RequestHandler):
-
-
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-    # ('/welcome', WelcomeHandler)
+    ('/', MainHandler),
+    ('/welcome', WelcomeHandler)
 ], debug=True)
